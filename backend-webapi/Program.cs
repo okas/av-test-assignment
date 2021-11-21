15a1151d6e9c5f9dc3521ff6b1e22ee6cf880c91
Lauri Saar <lauri.saar@outlook.com>
2021-11-21 17:43:41 +0000

    - Added XML document generation. - Added Swagger configuration: use .NET XML doc, include Controller XML docs. - Added sample summaries to test endpoint for POC.

--- backend-webapi/Program.cs
+++ backend-webapi/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+});
 
 var app = builder.Build();
 
