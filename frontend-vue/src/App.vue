b7b378d16540b7664fa05e47b13544635c10f49f
Lauri Saar <lauri.saar@outlook.com>
2021-11-21 16:44:10 +0000

    - Swagger uses now https - Added Swagger UI link to Home view.

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Algus</router-link> |
-    <router-link to="/about">Rakendusest</router-link>
+    <router-link to="/about">Rakendusest</router-link> |
+    <a class="external" href="https://localhost:5003/swagger" target="_blank">Swagger</a>
   </div>
   <router-view />
 </template>
@@ -27,4 +28,9 @@
       #nav a.router-link-exact-active {
         color: #42b983;
       }
+
+  a.external[target="_blank"]::after {
+    content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==);
+    margin: 0px 3px 0px 5px;
+  }
 </style>
