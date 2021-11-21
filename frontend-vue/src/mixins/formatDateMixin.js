cb490643506dd0955c07511a073ae5cc312ac21f
Lauri Saar <lauri.saar@outlook.com>
2021-11-21 15:32:47 +0000

    - If locale info is not provided, then defaults to navigator.languages list. - Added JSDoc info.

--- frontend-vue/src/mixins/formatDateMixin.js
+++ frontend-vue/src/mixins/formatDateMixin.js
@@ -1,6 +1,11 @@
 export default {
   methods: {
-    formatDate(date, locale) {
+    /**
+     * Format date using Intl API.
+     * @param {string|Number|Date} date Any Date constructor compatible parameter.
+     * @param {string|string[]} [locale=navigator.languages] Defaults to browser's language list.
+     */
+    formatDate(date, locale = navigator.languages) {
       const d = new Date(date);
       return new Intl.DateTimeFormat(locale, { dateStyle: 'short' }).format(d);
     }
