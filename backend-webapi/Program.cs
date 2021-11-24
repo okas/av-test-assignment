ea6daab2f45fafecce69f04e62b540e2745f3c55
Lauri Saar <lauri.saar@outlook.com>
2021-11-24 00:57:22 +0000

    For more beautiful API documentation and usage forcing following to lowercase: - URLs and quert sstrings, via Routing options - OAS tags, via custom Document filter.

--- backend-webapi/Program.cs
+++ backend-webapi/Program.cs
@@ -1,17 +1,26 @@
+using Backend.WebApi.Swagger;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<RouteOptions>(options =>
+{
+    // Client tooling feels less errorprone this way. Other than lowercase URLs are not nice.
+    options.LowercaseUrls = true;
+    options.LowercaseQueryStrings = true;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
+    // For .NET XML documentation inclusion to OAS documentation.
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+    c.DocumentFilter<LowerCaseTagsDocumentFilter>();
 });
 
 var app = builder.Build();
