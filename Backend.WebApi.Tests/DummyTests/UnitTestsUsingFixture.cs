66b5c1f85667aef18dbfc52865ba22823830bbfa
Lauri Saar <lauri.saar@outlook.com>
2021-11-25 20:45:10 +0000

    Poking around... Should delete in future.

---
+++ Backend.WebApi.Tests/DummyTests/UnitTestsUsingFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Backend.WebApi.Tests;
+
+public class UnitTestsUsingFixture : IClassFixture<MyTestFixture>
+{
+    public UnitTestsUsingFixture(MyTestFixture importantInstanceToTest) => ImportantInstanceToTest = importantInstanceToTest;
+
+    public MyTestFixture ImportantInstanceToTest { get; private set; }
+
+    [Fact]
+    public void IsEven_EvenValues_ReturnTrue()
+    {
+        var subject = new LogicToTest();
+        var result = ImportantInstanceToTest.Instance.EvenNumbers.All(subject.IsEven);
+        Assert.True(result, $"{{{string.Join(", ", ImportantInstanceToTest.Instance.EvenNumbers)}}}, but all should be even numbers.");
+    }
+
+    [Fact]
+    public void IsEven_OddValues_ReturnTrue()
+    {
+        var subject = new LogicToTest();
+        var result = !ImportantInstanceToTest.Instance.EvenNumbers.All(subject.IsEven);
+        Assert.False(result, $"{{{string.Join(", ", ImportantInstanceToTest.Instance.OddNumbers)}}}, but all should be odd numbers.");
+    }
+}
+
+
+public class MyTestFixture : IDisposable
+{
+    public MyTestFixture()
+    {
+        // Test fixture setup code
+        Instance = new SomeExpensiveTestResourceToShare(
+            new int[] { -2, 0, 2 },
+            new int[] { -1, 1, 3 }
+        );
+    }
+
+    public SomeExpensiveTestResourceToShare Instance { get; private set; }
+
+    public void Dispose()
+    {
+        // Test fixture teardown code
+        Instance.EvenNumbers.Clear();
+        Instance.OddNumbers.Clear();
+    }
+}
+
+public class SomeExpensiveTestResourceToShare
+{
+    public SomeExpensiveTestResourceToShare(int[] evenNumbers, int[] oddNumbers)
+    {
+        EvenNumbers = new List<int>(evenNumbers);
+        OddNumbers = new List<int>(oddNumbers);
+    }
+
+    public List<int> EvenNumbers { get; private set; }
+
+    public List<int> OddNumbers { get; private set; }
+}
+
+public class LogicToTest
+{
+    public bool IsEven(int candidate) => candidate % 2 == 0;
+}
--- Backend.WebApi.Tests/DummyTests/UnitTestsUsingTheories.cs
+++ Backend.WebApi.Tests/DummyTests/UnitTestsUsingTheories.cs
@@ -2,20 +2,16 @@ using Xunit;
 
 namespace Backend.WebApi.Tests;
 
-public class UnitTest1
+public class UnitTestsUsingTheories
 {
-    public static bool IsEven(int candidate)
-    {
-        return candidate % 2 == 0;
-    }
-
     [Theory]
     [InlineData(-2)]
     [InlineData(0)]
     [InlineData(2)]
-    public void IsEvenEvenValues_ReturnTrue(int value)
+    public void IsEven_EvenNumber_ReturnTrue(int value)
     {
-        var result = IsEven(value);
+        var subject = new LogicToTest();
+        var result = subject.IsEven(value);
         Assert.True(result, $"\"{value}\": Should be even number.");
     }
 
@@ -23,9 +19,10 @@ public class UnitTest1
     [InlineData(-1)]
     [InlineData(1)]
     [InlineData(3)]
-    public void IsEven_OddValues_ReturnFalse(int value)
+    public void IsEven_OddNumber_ReturnFalse(int value)
     {
-        var result = IsEven(value);
+        var subject = new LogicToTest();
+        var result = subject.IsEven(value);
         Assert.False(result, $"\"{value}\": Should be even number.");
     }
 }
