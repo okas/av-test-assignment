a2f1a790d4db2f79960f1e6c3bca0d1b1b3fbac2
Lauri Saar <lauri.saar@outlook.com>
2021-11-28 17:45:29 +0000

    Bugfix in EF migration: after files removing to new folder, namespace wasn't fixed in all subjected files.

--- backend-webapi/Data.EF/Migrations/20211124033944_Initial.Designer.cs
+++ backend-webapi/Data.EF/Migrations/20211124033944_Initial.Designer.cs
@@ -9,45 +9,45 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Backend.WebApi.Migrations
+namespace Backend.WebApi.Data.EF.Migrations;
+
+[DbContext(typeof(ApiDbContext))]
+[Migration("20211124033944_Initial")]
+partial class Initial
 {
-    [DbContext(typeof(ApiDbContext))]
-    [Migration("20211124033944_Initial")]
-    partial class Initial
+    protected void BuildTargetModel(ModelBuilder modelBuilder)
     {
-        protected void BuildTargetModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.0")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+        modelBuilder
+            .HasAnnotation("ProductVersion", "6.0.0")
+            .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+        SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
 
-            modelBuilder.Entity("Backend.WebApi.Model.UserInteraction", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
+        modelBuilder.Entity("Backend.WebApi.Model.UserInteraction", b =>
+            {
+                b.Property<Guid>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("uniqueidentifier");
 
-                    b.Property<DateTime>("Created")
-                        .HasColumnType("datetime2");
+                b.Property<DateTime>("Created")
+                    .HasColumnType("datetime2");
 
-                    b.Property<DateTime>("Deadline")
-                        .HasColumnType("datetime2");
+                b.Property<DateTime>("Deadline")
+                    .HasColumnType("datetime2");
 
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("Description")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.Property<bool>("IsOpen")
-                        .HasColumnType("bit");
+                b.Property<bool>("IsOpen")
+                    .HasColumnType("bit");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("UserInteraction");
-                });
+                b.ToTable("UserInteraction");
+            });
 #pragma warning restore 612, 618
-        }
     }
 }
+
