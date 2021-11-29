47900537129f786eba877429bf08b19c842287c8
Lauri Saar <lauri.saar@outlook.com>
2021-11-29 04:43:35 +0000

    Added API operation code for PATH, to close Interactions.

--- frontend-vue/src/views/UserInteractions.vue
+++ frontend-vue/src/views/UserInteractions.vue
@@ -15,7 +15,7 @@
             <th>Kirjeldus</th>
             <th>Sisestatud</th>
             <th>Tähtaeg</th>
-            <th>Avatud</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -23,7 +23,9 @@
             <td class="item-description">{{ item.description }}</td>
             <td>{{ formatDateTimeShortDateShortTime(item.created) }}</td>
             <td>{{ formatDateTimeShortDateShortTime(item.deadline) }}</td>
-            <td>{{ item.isOpen }}</td>
+            <td class="item-open">
+              <button id="checkbox" @click="markInteractionClosed(item)">✔</button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -32,86 +34,114 @@
 </template>
 
 <script>
-  import formatDateMixin from "../mixins/formatDateMixin";
+import formatDateMixin from "../mixins/formatDateMixin";
 
-  export default {
-    name: "UserInteractions",
-    mixins: [formatDateMixin],
-    data: () => ({ interactions: [] }),
-    beforeMount() {
-      this.getInteractions();
+export default {
+  name: "UserInteractions",
+  mixins: [formatDateMixin],
+  data: () => ({ interactions: [] }),
+  beforeMount() {
+    this.getInteractions();
+  },
+  methods: {
+    async getInteractions() {
+      const resp = await this.$api.then((client) =>
+        client.execute({ operationId: "get_api_userinteractions" })
+      );
+      if (resp.ok) {
+        this.interactions = resp.body.map(this.convertToVM);
+      }
     },
-    methods: {
-      async getInteractions() {
-        const resp = await this.$api.then((client) =>
-          client.execute({ operationId: "get_api_userinteractions" })
-        );
-        if (resp.ok) {
-          // Convert dates to JS Date instances from response.
-          this.interactions = resp.body.map(({ created, deadline, ...rest }) => ({
-            created: new Date(created),
-            deadline: new Date(deadline),
-            ...rest
-          }));
-        }
-      },
-      isProblematic(interaction) {
-        let toCompare = new Date();
-        toCompare.setHours(toCompare.getHours() + 1);
-        return interaction.deadline < toCompare;
-      },
+    async markInteractionClosed(itnteraction) {
+      if (!confirm("Kinnita pöördumise sulgemine")) {
+        return;
+      }
+      const resp = await this.$api.then((client) =>
+        client.execute({
+          operationId: "patch_api_userinteractions__id_",
+          parameters: { id: itnteraction.id },
+          requestBody: { id: itnteraction.id, isOpen: false },
+        })
+      );
+      if (resp.ok) {
+        this.interactions.splice(this.interactions.indexOf(itnteraction), 1);
+      } else {
+        alert(resp);
+      }
     },
-    watch: {
-      "this.interactions"() {
-        // Keep table sorted by deadline always.
-        this.interactions.sort((a, b) => a.deadline - b.deadline);
-      },
+    /** Convert dates to JS Date instances from response. */
+    convertToVM({ created, deadline, ...rest }) {
+      return {
+        created: new Date(created),
+        deadline: new Date(deadline),
+        ...rest,
+      };
     },
-  };
+    isProblematic(interaction) {
+      let toCompare = new Date();
+      toCompare.setHours(toCompare.getHours() + 1);
+      return interaction.deadline < toCompare;
+    },
+    toYesNo(interaction) {
+      return interaction.isOpen ? "jah" : "ei";
+    },
+  },
+  watch: {
+    /** Keep table sorted by deadline desc. always.*/
+    "this.interactions"() {
+      this.interactions.sort((a, b) => a.deadline - b.deadline);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  table {
-    table-layout: auto;
-    border-spacing: 0px;
-    border: 1px #42b983 solid;
-    width: 100%;
-  }
+table {
+  table-layout: auto;
+  border-spacing: 0px;
+  border: 1px #42b983 solid;
+  width: 100%;
+}
 
-  td, th {
-    padding: 0.75rem 0.5rem;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-  }
+td,
+th {
+  padding: 0.75rem 0.5rem;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
 
-  td {
-    text-align: right;
-  }
+td {
+  text-align: right;
+}
 
-  th {
-    border-bottom: 1px #42b983 solid;
-  }
+th {
+  border-bottom: 1px #42b983 solid;
+}
 
-  tbody tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
+tbody tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
 
-  tbody tr:hover {
-    background: #daf1f1;
-    outline: 1px solid black;
-  }
+tbody tr:hover {
+  background: #daf1f1;
+  outline: 1px solid black;
+}
 
-  .item-description {
-    text-align: justify;
-  }
+.item-description {
+  text-align: justify;
+}
 
-  tbody tr.due-problem {
-    outline: 1px solid red;
-    background-color: #ffe6e6;
-  }
+.item-open {
+  text-align: center;
+}
 
-  tbody tr.due-problem:hover {
-    outline: 1px solid black;
-  }
+tbody tr.due-problem {
+  outline: 1px solid red;
+  background-color: #ffe6e6;
+}
+
+tbody tr.due-problem:hover {
+  outline: 1px solid black;
+}
 </style>
