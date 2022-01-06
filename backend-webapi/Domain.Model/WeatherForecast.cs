e69a6a8da0a29431ae76091f40886496eadad46f
Lauri Saar <lauri.saar@outlook.com>
2022-01-06 13:27:37 +0000

    REFACTOR: testing model optimize

--- backend-webapi/Domain.Model/WeatherForecast.cs
+++ backend-webapi/Domain.Model/WeatherForecast.cs
@@ -1,12 +1,10 @@
 namespace Backend.WebApi.Domain.Model;
 
-public class WeatherForecast
+public readonly record struct WeatherForecast(
+    DateTime Date,
+    int TemperatureC,
+    string? Summary
+    )
 {
-    public DateTime Date { get; set; }
-
-    public int TemperatureC { get; set; }
-
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
 }
