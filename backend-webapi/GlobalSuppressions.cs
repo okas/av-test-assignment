a076b77bbc723e02477d93e36d7bf815c8468b48
Lauri Saar <lauri.saar@outlook.com>
2022-01-08 12:46:15 +0000

    REFACTOR: code style supression for generated file

--- backend-webapi/GlobalSuppressions.cs
+++ backend-webapi/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Design", "MA0048:File name must match type name", Justification = "<Pending>", Scope = "type", Target = "~T:Backend.WebApi.Infrastructure.Data.EF.Migrations.Initial")]
+[assembly: SuppressMessage("Style", "MA0007:Add a comma after the last value", Justification = "<Pending>", Scope = "member", Target = "~M:Backend.WebApi.Infrastructure.Data.EF.Migrations.Initial.Up(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)")]
