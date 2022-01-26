05b255de416f389bc789f6b434c9e00749572295
Lauri Saar <lauri.saar@outlook.com>
2022-01-26 21:07:21 +0000

    CHANGE: UserInteraction.Description attribute has MaxLength 300 in SQL

---
+++ backend-webapi/Infrastructure.Data.EF/Migrations/20220126210313_UserInteraction_Description_HasMaxLen300.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.WebApi.Infrastructure.Data.EF.Migrations
+{
+    public partial class UserInteraction_Description_HasMaxLen300 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "UserInteraction",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "UserInteraction",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
