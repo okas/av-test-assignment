3a176ae645143201b0659d3e36c77b0b859ae3d7
Lauri Saar <lauri.saar@outlook.com>
2022-01-27 12:48:36 +0000

    REFACTOR: moved supression from attribute to code line level, because attribute silences waning for entire member

--- backend-webapi/App.Operations/UserInteractionCommands/UserInteractionCreateCommand.cs
+++ backend-webapi/App.Operations/UserInteractionCommands/UserInteractionCreateCommand.cs
@@ -28,7 +28,6 @@ public readonly record struct UserInteractionCreateCommand(
         /// <inheritdoc />
         /// <exception cref="AlreadyExistsException" />
         /// <exception cref="DbUpdateConcurrencyException" />
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "MA0042:Do not use blocking calls in an async method", Justification = "Context Add method is enough as this current DL config do not use Hi/Lo key gen for current entity.")]
         public async Task<UserInteractionDto> Handle(UserInteractionCreateCommand rq, CancellationToken ct)
         {
             UserInteraction model = new()
@@ -41,7 +40,9 @@ public readonly record struct UserInteractionCreateCommand(
 
             try
             {
+#pragma warning disable MA0042 // Do not use blocking calls in an async method
                 _context.UserInteraction.Add(model);
+#pragma warning restore MA0042 // Do not use blocking calls in an async method
                 await _context.SaveChangesAsync(ct).ConfigureAwait(false);
 
                 _logger.InformCreated(new { model.Id });
