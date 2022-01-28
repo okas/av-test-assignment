94273168741c4fac6f9518a911bdd337f4d64c19
Lauri Saar <lauri.saar@outlook.com>
2022-01-28 11:17:11 +0000

    REFACTOR_ code simplification

--- backend-webapi/App.Operations/UserInteractionCommands/UserInteractionUpdateCommand.cs
+++ backend-webapi/App.Operations/UserInteractionCommands/UserInteractionUpdateCommand.cs
@@ -6,7 +6,6 @@ using Backend.WebApi.Domain.Model;
 using Backend.WebApi.Infrastructure.Data.EF;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Backend.WebApi.App.Operations.UserInteractionCommands;
 
@@ -31,34 +30,17 @@ public readonly record struct UserInteractionUpdateCommand(
         public Handler(ApiDbContext context, ILogger<Handler> logger) => (_context, _logger) = (context, logger);
 
         /// <inheritdoc />
-        /// <inheritdoc cref="GetModelAsync"/>
-        /// <inheritdoc cref="TrySaveAsync"/>
-        public async Task<Unit> Handle(UserInteractionUpdateCommand rq, CancellationToken ct)
-        {
-            UserInteraction model = await GetModelAsync(rq, ct).ConfigureAwait(false);
-
-            EntityEntry<UserInteraction> entry = _context.Entry(model);
-
-            entry.CurrentValues.SetValues(rq);
-
-            await TrySaveAsync(entry, ct).ConfigureAwait(false);
-
-            _logger.InformUpdated(new { model.Id });
-
-            return Unit.Value;
-        }
-
-        /// <exception cref="NotFoundException" />
-        private async Task<UserInteraction> GetModelAsync(UserInteractionUpdateCommand rq, CancellationToken ct) =>
-            await _context.UserInteraction.FindAsync(new object[] { rq.Id }, ct).ConfigureAwait(false)
-                ?? throw new NotFoundException("Update operation cancelled.", new { rq.Id }, _logCategory);
-
         /// <exception cref="NotFoundException" />
         /// <exception cref="ConcurrentUpdateException" />
         /// <exception cref="OperationCanceledException" />
         /// <exception cref="DbUpdateException" />
-        private async Task TrySaveAsync(EntityEntry<UserInteraction> entry, CancellationToken ct)
+        public async Task<Unit> Handle(UserInteractionUpdateCommand rq, CancellationToken ct)
         {
+            UserInteraction entity = await _context.UserInteraction.FindAsync(new object[] { rq.Id }, ct).ConfigureAwait(false)
+                ?? throw new NotFoundException("Update operation cancelled.", new { rq.Id }, _logCategory);
+
+            _context.Entry(entity).CurrentValues.SetValues(rq);
+
             try
             {
                 await _context.SaveChangesAsync(ct).ConfigureAwait(false);
@@ -66,12 +48,16 @@ public readonly record struct UserInteractionUpdateCommand(
             catch (DbUpdateConcurrencyException)
             {
                 const string message = "Concurrency detected, operation cancelled.";
-                var model = new { entry.Entity.Id };
+                var model = new { entity.Id };
 
-                throw await _context.UserInteraction.AnyAsync(e => e.Id == entry.Entity.Id, CancellationToken.None).ConfigureAwait(false)
+                throw await _context.UserInteraction.AnyAsync(e => e.Id == entity.Id, CancellationToken.None).ConfigureAwait(false)
                     ? new ConcurrentUpdateException(message, model, _logCategory)
                     : new NotFoundException(message, model, _logCategory);
             }
+
+            _logger.InformUpdated(new { entity.Id });
+
+            return Unit.Value;
         }
     }
 }
