690e60f900b71609a92217474a2dda7973d0108e
Lauri Saar <lauri.saar@outlook.com>
2022-02-04 19:05:29 +0000

    STEP #7: Add .vue extension to SFC imports
    - none found for current project.
    
    STEP #8: Clean Up Magic Comments

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -10,19 +10,12 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
   {
     path: "/userinteractions",
     name: "UserInteractions",
-    component: () =>
-      import(
-        /* webpackChunkName: "userinteractions" */ "../views/UserInteractions.vue"
-      ),
+    component: () => import("../views/UserInteractions.vue"),
   },
 ];
 
