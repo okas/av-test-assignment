14908d1b02a53d695cddcb0763c84a3fc5ce788f
Lauri Saar <lauri.saar@outlook.com>
2022-02-05 22:32:21 +0000

    REFACTOR: translatorPlugin internaly uses new dynamic import features
    - API did not change for consumers.
    
    CHANGE: translatorPlugin now has configuration
    
    - allows to specify translations root folder;
    - plugin API did not changes otherwise.

--- frontend-vue/src/plugins/translatorPlugin/index.js
+++ frontend-vue/src/plugins/translatorPlugin/index.js
@@ -1,33 +1,45 @@
 import { inject } from "vue";
 
 /**
+ * @typedef TranslatorConfig Translator options
+ * @type {object} 
+ * @property {string} [rootFolder=translations] Root path of translations in `/src` folder
+ */
+
+/**
+ * @param {string} templateRoot
  * @param {string} modulePath
  * @returns {Promise<Object>}
  */
-async function translationResolverAsync(modulePath, language = "") {
+async function translationResolverAsync(templateRoot, modulePath, language = "") {
   let modulePromise;
 
-  // Import expressions static prefixes will be bundeled to chunks based WebPack static analyzer.
+  // Consider: https://github.com/tc39/proposal-dynamic-import#import
+  // Consider: https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
   switch (language) {
     default:
     case "en":
-      modulePromise = () => import("../../translations/en/" + modulePath);
+      modulePromise = import(`../../${templateRoot}/en/${modulePath}.js`);
       break;
     case "et":
-      modulePromise = () => import("../../translations/et/" + modulePath);
+      modulePromise = import(`../../${templateRoot}/et/${modulePath}.js`);
       break;
   }
 
-  return (await modulePromise()).default;
+  const module = await modulePromise;
+
+  return module.default;
 }
 
 export const pluginSymbol = Symbol("translator plugin: resolver symbol");
 
 /**
  * @param {import("vue").App<any>} app
+ * @param {TranslatorConfig} config
  */
-export default function install(app) {
+export default function install(app, config = { rootFolder: "translations" }) {
   const { globalProperties } = app.config;
+  const root = config?.rootFolder ?? "translations";
   // For Options API users.
   globalProperties.$translatorResolverAsync = async (
     /** @type {string} */ modulePath,
@@ -38,7 +50,7 @@ export default function install(app) {
         ? language
         : globalProperties.$store?.state?.language ?? "";
 
-    return await translationResolverAsync(modulePath, lng);
+    return await translationResolverAsync(root, modulePath, lng);
   };
   // For Composition API users.
   app.provide(pluginSymbol, globalProperties.$translatorResolverAsync);
