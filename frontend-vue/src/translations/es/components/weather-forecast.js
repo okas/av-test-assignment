bc67d42e03dd9c1817790fb423056d7003fd91a1
Lauri Saar <lauri.saar@outlook.com>
2022-02-08 22:07:56 +0000

    ADD: Spanish translation

---
+++ frontend-vue/src/translations/es/components/weather-forecast.js
@@ -0,0 +1,4 @@
+export default {
+  header: ["Pronóstico del tiempo", "(API generado)"],
+  tableHeader: ["Fecha", "Temperatura °C", "Temperatura °F", "Descripción"],
+};
---
+++ frontend-vue/src/translations/es/views/UserInteractions.js
@@ -0,0 +1,12 @@
+export default {
+  header: "Vista de peticiones de servicio",
+  section_form: {
+    description_placeholder: "descriptción",
+    deadline_label: "plazo",
+    submit_text: "añadir",
+  },
+  section_list: {
+    header: "Peticiones abiertas",
+    table_header: ["Descriptción", "Creado", "Plazo"],
+  },
+};
