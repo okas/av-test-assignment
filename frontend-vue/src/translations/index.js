62980a122b685cacff38dfff1eedd6d951f84ac5
Lauri Saar <lauri.saar@outlook.com>
2022-02-08 15:50:45 +0000

    FEAT: created LanguageSelector component and implemented it in AppRoot.vue
    - simple <select> based UI;
    - intermediates store value "app.language";
    - allows to select language;
    - shows selected language;
    - sets DOM <html lang> attribute value

---
+++ frontend-vue/src/translations/index.js
@@ -0,0 +1,20 @@
+export const supportedLanguages = [
+  {
+    iso: "es",
+    native: "Español",
+    label: "Idioma",
+    auxilliar: "elija su idioma",
+  },
+  {
+    iso: "en",
+    native: "English",
+    label: "Language",
+    auxilliar: "pick a language",
+  },
+  {
+    iso: "et",
+    native: "Eesti",
+    label: "Keel",
+    auxilliar: "vali keel",
+  },
+];
