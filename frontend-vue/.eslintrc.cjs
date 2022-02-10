3b3ea7fc8e42f2e0f2f1e5899d2c5e5610f89ec9
Lauri Saar <lauri.saar@outlook.com>
2022-02-10 11:42:03 +0000

    CHANGE: added ESLint-Typescript for better DX
    - Vue scpecific config added

--- frontend-vue/.eslintrc.cjs
+++ frontend-vue/.eslintrc.cjs
@@ -7,9 +7,12 @@ module.exports = {
     "vue/setup-compiler-macros": true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: 2022,
+    parser: "@typescript-eslint/parser",
   },
+  plugins: ["@typescript-eslint"],
   rules: {
     "no-console": process.env.MODE === "production" ? "warn" : "off",
     "no-debugger": process.env.MODE === "production" ? "warn" : "off",
