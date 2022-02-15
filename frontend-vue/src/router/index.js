dab615fced142811842ee4e31282de9e95cef67d
Lauri Saar <lauri.saar@outlook.com>
2022-02-15 12:09:28 +0000

    CHANGE: all the routes use now dynamic imports
    REFACTOR: moved raw routes (options) to separate moduel

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,27 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { rawRouteOptions } from "./routes";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue"),
-  },
-  {
-    path: "/userinteractions",
-    name: "UserInteractions",
-    component: () => import("../views/UserInteractions.vue"),
-  },
-];
+// Apply dynamic loading for all the routes
+rawRouteOptions.forEach((route) => {
+  let viewPath = route.component;
+  const start = viewPath.startsWith("../") ? 2 : 0;
+  const end = viewPath.endsWith(".vue") ? viewPath.length - 4 : viewPath.length;
+  viewPath = viewPath.substring(start, end);
+  // Start and extension must be static strings for Rollup dynamic import!
+  route.component = () => import(`../${viewPath}.vue`);
+});
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: rawRouteOptions,
 });
 
 export default router;
