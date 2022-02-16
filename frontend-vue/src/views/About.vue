162592671be8ded05ed6683fbea8bfd2ca0bacc0
Lauri Saar <lauri.saar@outlook.com>
2022-02-16 10:03:58 +0000

    REFACTOR: migrated About views to use Tree component
    - changed translation files structure for more accurate HTML structure reflection.

--- frontend-vue/src/views/About.vue
+++ frontend-vue/src/views/About.vue
@@ -1,33 +1,6 @@
 <template>
   <article class="about">
-    <section>
-      <header>
-        <h1>{{ translationVm.section[0].header }}</h1>
-      </header>
-      <p>{{ translationVm.section[0].p1 }}</p>
-      <ol>
-        <li v-for="(item, i) in translationVm.section[0].list1" :key="i">
-          {{ item }}
-        </li>
-      </ol>
-      <p>{{ translationVm.section[0].p2 }}</p>
-    </section>
-    <section>
-      <header>
-        <h1>{{ translationVm.section[1].header }}</h1>
-      </header>
-      <p>{{ translationVm.section[1].p1 }}</p>
-      <p>{{ translationVm.section[1].p2 }}</p>
-      <ol>
-        <li v-for="(item, i) in translationVm.section[1].list1" :key="i">
-          {{ item }}
-        </li>
-      </ol>
-    </section>
-    <section>
-      <header><h1>Testing</h1></header>
-      <Tree :data="testTree" />
-    </section>
+    <Tree :data="translationVm" />
   </article>
 </template>
 
@@ -37,54 +10,8 @@ import { useTranslator } from "../plugins/translatorPlugin";
 import useRootStore from "../stores/app-store";
 
 const Tree = defineAsyncComponent(() => import("../components/tree.vue"));
-
-const testTree = [
-  [
-    {
-      article: {
-        section: {
-          p: "list subject",
-          span: ["span1", "span2"],
-          div: {
-            p: "section>div>p",
-            span: ["section>div>p>span1", "section>div>p>span2"],
-          },
-          div_1: ["div1", "div2"],
-        },
-        section_1: [
-          {
-            div: {
-              p: "section_1>div>p",
-              p_1: ["section_1>div>p[0]", "section_1>div>p[1]"],
-            },
-          },
-        ],
-        ol: {
-          li: ["ol>li[0]", "ol>li[1]"],
-        },
-      },
-    },
-  ],
-];
-
 const store = useRootStore();
-
-const translationVm = ref({
-  section: [
-    {
-      header: "",
-      p1: "",
-      list1: [],
-      p2: "",
-    },
-    {
-      header: "",
-      p1: "",
-      p2: "",
-      list1: [],
-    },
-  ],
-});
+const translationVm = ref(null);
 
 const translatorAsync = useTranslator();
 
