0f964556c6323749c6daf12545a7feb0ce6ecad5
Lauri Saar <lauri.saar@outlook.com>
2022-02-20 18:00:33 +0000

    ADD: simple modal, experimetn

--- frontend-vue/src/views/UserInteractions.vue
+++ frontend-vue/src/views/UserInteractions.vue
@@ -4,7 +4,57 @@
       <h1>{{ translatedVm.header }}</h1>
     </header>
     <section>
+      <modal ref="modalName">
+        <template #header>
+          <h1>Modal title</h1>
+        </template>
+
+        <template #body>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed
+            velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus
+            proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor
+            purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id.
+            Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices
+            gravida dictum fusce ut placerat orci. Tristique magna sit amet
+            purus. Fermentum et sollicitudin ac orci phasellus egestas tellus.
+            Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan.
+            Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget
+            nullam non nisi est sit amet facilisis. Mi in nulla posuere
+            sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis
+            nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque
+            viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et
+            tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a.
+            Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec
+            feugiat nisl pretium fusce.
+          </p>
+          <p>
+            Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl
+            nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum.
+            Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris
+            sit amet massa vitae tortor condimentum lacinia. Eu non diam
+            phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus
+            non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis
+            feugiat vivamus at augue eget. Nibh tellus molestie nunc non
+            blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam
+            nulla facilisi cras fermentum odio eu feugiat pretium. Mattis
+            aliquam faucibus purus in massa.
+          </p>
+        </template>
+
+        <template #footer>
+          <div>
+            <button @click="$refs.modalName.closeModal()">Cancel</button>
+            <button @click="$refs.modalName.closeModal()">Save</button>
+          </div>
+        </template>
+      </modal>
+
       <form class="form" @submit.prevent>
+        <div class="control">
+          <button @click="$refs.modalName.openModal">℗</button>
+        </div>
         <div class="control">
           <input
             id="new-description"
@@ -103,6 +153,7 @@ import { useTranslator } from "../plugins/translatorPlugin";
 import { useApiClient } from "../plugins/swaggerClientPlugin";
 import useFormatDateTime from "../utils/formatDateTime";
 import useRootStore from "../stores/app-store";
+import Modal from "../components/modal.vue";
 
 const store = useRootStore();
 const translatedVm = ref({
@@ -213,6 +264,12 @@ function addNewInteraction({ description, deadline }) {
     });
 }
 </script>
+
+<style>
+.overflow-hidden {
+  overflow: hidden;
+}
+</style>
 
 <style scoped>
 table {
