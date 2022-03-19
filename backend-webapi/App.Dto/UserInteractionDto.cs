8a5b1bfbfe3d985f61c4b8ee944ecf1989152dd7
Lauri Saar <lauri.saar@outlook.com>
2022-03-19 11:56:40 +0000

    CHANGE: added RowVersion mapping to DTO class

--- backend-webapi/App.Dto/UserInteractionDto.cs
+++ backend-webapi/App.Dto/UserInteractionDto.cs
@@ -6,12 +6,19 @@ namespace Backend.WebApi.App.Dto;
 /// <summary>
 /// Userinteraction DTO with full data. For full detail presentation purpose of the model entity.
 /// </summary>
+/// <param name="Id"></param>
+/// <param name="Description"></param>
+/// <param name="Deadline"></param>
+/// <param name="Created"></param>
+/// <param name="IsOpen"></param>
+/// <param name="ETag">Base64 string made from <see cref="T:System.Byte[]"/> array.</param>
 public readonly record struct UserInteractionDto(
     Guid Id,
     string Description,
     DateTime Deadline,
     DateTime Created,
-    bool IsOpen
+    bool IsOpen,
+    string ETag
     )
 {
     public static Expression<Func<UserInteraction, UserInteractionDto>> Projection => (model) => new()
@@ -21,5 +28,6 @@ public readonly record struct UserInteractionDto(
         Deadline = model.Deadline,
         Created = model.Created,
         IsOpen = model.IsOpen,
+        ETag = Convert.ToBase64String(model.RowVer),
     };
 }
