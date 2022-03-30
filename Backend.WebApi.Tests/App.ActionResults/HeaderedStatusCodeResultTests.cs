86affac3977790ab73918f065bfc7d7b0e5b1108
Lauri Saar <lauri.saar@outlook.com>
2022-03-30 20:47:24 +0100

    FEAT: added new result type HeaderedStatusCodeResult: ActionResult
    - HTTP sets status code and provided headers;
    - will not set any HTTP content / body.

---
+++ Backend.WebApi.Tests/App.ActionResults/HeaderedStatusCodeResultTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture.Xunit2;
+using Backend.WebApi.App.ActionResults;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Xunit;
+
+namespace Backend.WebApi.Tests.App.ActionResults;
+
+[Collection("ActionExecutionFixture")]
+public sealed class HeaderedStatusCodeResultTests
+{
+    private readonly ActionContext _actionContext;
+
+    public HeaderedStatusCodeResultTests(ActionExecutionFixture fixture) =>
+        _actionContext = fixture.CreateActionContext();
+
+    [Theory]
+    [AutoMoqData(true)] // NB! OmitAutoProperty filling of Header auto-prop.
+    public void ExecuteResult_StateUnderTest_ExpectedBehavior(
+        // Arrange
+        [Frozen(Matching.ParameterName)] int statusCode,
+        [Frozen(Matching.ParameterName)] KeyValuePair<string, StringValues> header,
+        [Frozen(Matching.ParameterName)] KeyValuePair<string, StringValues>[] headers,
+        HeaderedStatusCodeResult sutActionResult)
+    {
+        IEnumerable<KeyValuePair<string, StringValues>>? allHeaders = headers.Prepend(header);
+
+        // Act
+        sutActionResult.ExecuteResult(
+            _actionContext
+            );
+
+        // Assert
+        using AssertionScope _ = new();
+
+        _actionContext.HttpContext.Response.StatusCode.Should().Be(statusCode);
+
+        _actionContext.HttpContext.Response.Headers.Should().BeEquivalentTo(allHeaders);
+    }
+}
---
+++ backend-webapi/App.ActionResults/HeaderedStatusCodeResult.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace Backend.WebApi.App.ActionResults;
+
+/// <summary>
+/// Takes in HTTP status code 1..* headers, but will not set the HTTP content.
+/// Renders <see cref="HttpResponse"/> without setting the <see cref="HttpResponse.Body"/>.
+/// </summary>
+public class HeaderedStatusCodeResult : ActionResult, IStatusCodeActionResult
+{
+    /// <param name="statusCode">Recommended: use <see cref="StatusCodes"/>.</param>
+    /// <param name="header">Recommended: use <see cref="HeaderNames"/>.</param>
+    /// <param name="headers">Recommended: use <see cref="HeaderNames"/>.</param>
+    public HeaderedStatusCodeResult([ActionResultStatusCode] int statusCode,
+        KeyValuePair<string, StringValues> header,
+        params KeyValuePair<string, StringValues>[] headers)
+    {
+        StatusCode = statusCode;
+
+        IEnumerable<KeyValuePair<string, StringValues>> mergedHeaders = headers.Prepend(header);
+
+        Headers = new HeaderDictionary(new Dictionary<string, StringValues>(mergedHeaders, StringComparer.Ordinal));
+    }
+
+    public int? StatusCode { get; init; }
+
+    public IHeaderDictionary Headers { get; init; }
+
+    public override void ExecuteResult(ActionContext context)
+    {
+        if (context == null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        context.HttpContext.Response.StatusCode = StatusCode!.Value;
+
+        foreach (KeyValuePair<string, StringValues> header in Headers)
+        {
+            context.HttpContext.Response.Headers.Add(header);
+        }
+    }
+}
