520f3d7f902490185a7d7e3a56efbdc4caacd69e
Lauri Saar <lauri.saar@outlook.com>
2022-04-12 00:33:44 +0100

    ADD: new derived specific AutoFixture data attr.:
    - It adds type relays for cache service support.

---
+++ Backend.WebApi.Tests/AutoMoqDataConditionalHttpRequestAttribute.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using Backend.WebApi.App.Cache;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Backend.WebApi.Tests;
+
+public class AutoMoqDataConditionalHttpRequestAttribute : AutoMoqDataAttribute
+{
+    public AutoMoqDataConditionalHttpRequestAttribute(bool omitAutoProperties = false)
+        : base(ConfigureFixture(GetFixtureFactory(omitAutoProperties)()))
+    { }
+
+    private static Func<IFixture> ConfigureFixture(IFixture fixture) => () =>
+    {
+        fixture.Customizations.Add(new TypeRelay(typeof(IMemoryCache), typeof(MemoryCache)));
+        fixture.Customizations.Add(new TypeRelay(typeof(ICacheService<>), typeof(MemoryCacheService<>)));
+
+        return fixture;
+    };
+}
